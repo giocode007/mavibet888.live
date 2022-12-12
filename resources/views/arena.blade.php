55f9c85724e904d82a193dc18a9277fb2bca8053
giocode007 <giocode007@gmail.com>
2022-12-12 19:17:52 +0800

    commit

--- resources/views/arena.blade.php
+++ resources/views/arena.blade.php
@@ -42,3 +42,92 @@
         </div>
     </div>
 @endsection
+@push('scripts')
+<script>
+    window.onload = function(){
+        var tbl = new weirdTable('table');
+
+        var _randomLoop = parseInt(Math.random() * 20);
+        console.log(_randomLoop);
+
+        var _resultList = [];
+
+        for(var i = 0; i < _randomLoop; i++){
+            _resultList.push(parseInt(Math.random() * 3));
+        }
+
+        tbl.addData(_resultList);
+    }
+
+
+    function weirdTable(tableId){
+        var _me = null;
+        
+        var _currentIndex = 0;
+        var _colCount     = 0;
+        var _lastRowIndex = 0;
+        var _lastResult = 0;
+        
+        var construct = function(tableId){
+            _me = document.getElementById(tableId);
+            _colCount = _me.rows[0].cells.length;
+            _currentIndex = _colCount;
+        };
+        
+        this.addData = function(data){
+
+            var row = _me.rows[_lastRowIndex];
+            
+            //or var data = arguments;
+            for(var i = 0; i < data.length; i++){
+
+                console.log(data[i] + " == " + _lastResult);
+
+                row = _me.insertRow(_lastRowIndex);
+
+                if(data[i] == _lastResult){
+                    console.log("InsertRow");
+                    _lastRowIndex++;
+                    row = _me.insertRow(_lastRowIndex);
+                    for(var i = 1; i < _currentIndex; i++){
+                        row.insertCell( i - 1 ).innerText = ' ';
+                    }
+                }else{
+                    row.insertCell(_currentIndex).innerText = data[i];
+
+_currentIndex++;
+                }
+
+                console.log("InsertCell");
+                console.log("_currentIndex " + _currentIndex + " , " + _lastRowIndex);
+
+                
+
+                _lastResult = data[i];
+
+
+                // if(_lastResult == data[i]){
+                //     _currentIndex = 0;
+                //     row = _me.insertRow(_lastRowIndex);
+                //     console.log('inserRow');
+                // }else{
+                // }
+            
+                // console.log('insertCell');
+
+                // row.insertCell(_currentIndex).innerText = data[i];
+                // _lastResult = data[i];
+                // console.log("last result " + _lastResult);
+                // _currentIndex++;
+                // _lastRowIndex++;
+
+
+                // console.log(_currentIndex + " , " + _lastRowIndex)
+            }
+        };
+        
+        construct(tableId);
+    }
+    
+</script>
+@endpush
