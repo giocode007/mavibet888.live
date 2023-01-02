3576fe41e3e36c4233057e39fec2378ea605e3a6
giocode007 <giocode007@gmail.com>
2023-01-02 20:28:19 +0800

    'Localhost'

--- public/js/app.js
+++ public/js/app.js
@@ -2163,13 +2163,13 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "mavibet_key",
+  key: "local",
   cluster: "mt1",
-  forceTLS: false,
-  wsHost: window.location.hostname,
+  wsHost: "127.0.0.1",
   wsPort: 6001,
-  encrypted: false,
-  enabledTransports: ['ws', 'wss']
+  forceTLS: false,
+  disableStats: true,
+  scheme: "http"
 });
 
 /***/ }),
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -37,9 +37,9 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: false,
-    wsHost: window.location.hostname,
+    wsHost: process.env.MIX_PUSHER_HOST,
     wsPort: 6001,
-    encrypted: false,
-    enabledTransports: ['ws', 'wss']
+    forceTLS: false,
+    disableStats: true,
+    scheme: process.env.MIX_PUSHER_SCHEME,
 });
